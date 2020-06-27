5b3b1b7bb162c36fe39c3b6d997e3ea71a6bd2ae
Aika Okemoto <aika2004oke@gmail.com>
2020-06-27 18:50:20 +0900

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by 桶本あいか on 2020/06/27.
+//  Copyright © 2020 Okemoto.Aika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
